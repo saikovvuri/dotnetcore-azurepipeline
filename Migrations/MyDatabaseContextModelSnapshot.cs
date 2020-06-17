07fa1e8462d185bd918bfdec66497b8bbfac9f77
Sai Kovvuri <sakovvu@microsoft.com>
2020-06-16 21:25:40 -0500

    add migration scripts

--- Migrations/MyDatabaseContextModelSnapshot.cs
+++ Migrations/MyDatabaseContextModelSnapshot.cs
@@ -23,7 +23,7 @@ namespace DotNetCoreSqlDb.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<DateTime>("CreatedDate")
-                        .HasColumnType("TEXT");
+                        .HasColumnType("NVARCHAR");
 
                     b.Property<string>("Description")
                         .HasColumnType("TEXT");
